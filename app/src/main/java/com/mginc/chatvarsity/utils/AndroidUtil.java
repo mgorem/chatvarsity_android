8ee529adaf0b168cee1ab663516f39fe0e2aa1cb
/omg <oremgitonga22@gmail.com>
2023-05-29 16:03:40 +0300

    sending otp successfully next verify otp, proceed to next screen

---
+++ app/src/main/java/com/mginc/chatvarsity/utils/AndroidUtil.java
@@ -0,0 +1,11 @@
+package com.mginc.chatvarsity.utils;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class AndroidUtil {
+
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+}
